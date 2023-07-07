2315bd02479b5f654a9b7eb40ba64c499842ad03
neeraj <me.neeraj1455@gmail.com>
2023-07-07 17:29:29 +0530

    initial commit

---
+++ blogApi/app.js
@@ -0,0 +1,58 @@
+/** @format */
+
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require(`mongoose`);
+require(`dotenv`).config();
+
+// routes
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var blogsRouter = require("./routes/blogs");
+
+// connect database
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log(`database connected`))
+  .catch(error => console.log(error.message));
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+// middleware
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// use routes
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/blogs", blogsRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
