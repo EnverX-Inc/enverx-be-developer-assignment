a88b900b4ef63b8f3e0b117cc23d0f1500bfa3f7
Urvish Rupareliya <urvishrupareliya@gmail.com>
2023-07-21 18:51:30 +0530

    add midalware

--- src/middleware/createPostValidation.js
+++ src/middleware/createPostValidation.js
@@ -1,11 +1,10 @@
 const { body } = require('express-validator');
-const { model, models } = require('mongoose');
 
 // Middleware for data validation
 const validateReqData = [
     body('title').trim().isLength({ min: 1 }).withMessage('Title must not be empty.'),
     body('id').isInt().withMessage('ID must be an integer.'),
     body('description').trim().isLength({ min: 1 }).withMessage('Description must not be empty.')
-  ];
+];
 
 module.exports = validateReqData;
